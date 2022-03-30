99dcafd64c6e7fe0d8c05326cc000a94dc8ba3a5
Alex Poirier <alexmatthewpoirier@gmail.com>
2022-03-30 14:19:49 -0400

    initial commit

---
+++ first-typescript-app/js/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var helloWorld = function () {
+    console.log("Hello Typescript World!");
+};
+helloWorld();
+var add = function (x, y) {
+    console.log(x + y);
+};
+add(2, 3);
+add(12, 34);
+var a = 44;
+var b = 6;
+var c = 888;
